35365f56c4afc17a7eda3274a13fc37cbc306fbe
Marcos Nunes <marcosnunesmbs@gmail.com>
2025-05-01 00:47:34 -0300

    Remove Dockerfile and .dockerignore; update SQL generation description for clarity

--- tools/generateSql.js
+++ tools/generateSql.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default {
     name: "generate_sql",
-    description: "Gera SQL da tabela loja com base na pergunta",
+    description: "Gera SQL da tabela loja com base na pergunta ou comando do usuário. Considere comando ou pergunta como sendo a mesma coisa (pergunta).",
     parameters: {
         type: "object",
         properties: {
