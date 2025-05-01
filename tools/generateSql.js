eff9c1454e447d73562bd4aac4d7d2c05c81666f
Marcos Nunes <marcosnunesmbs@gmail.com>
2025-05-01 17:02:07 -0300

    Move schema file path declaration to the top for improved readability

--- tools/generateSql.js
+++ tools/generateSql.js
@@ -1,10 +1,11 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
-const schemaFilePath = path.resolve(__dirname, './database_schema.txt');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const schemaFilePath = path.resolve(__dirname, './database_schema.txt');
 
 export default {
     name: "generate_sql",
