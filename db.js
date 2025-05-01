cb31ee9762b3d5a3cfaefff9a09bcfb2e7ae7a5e
Marcos Nunes <marcosnunesmbs@gmail.com>
2025-05-01 12:49:31 -0300

    Refactor database connection handling to be inside the query function for improved encapsulation

--- db.js
+++ db.js
@@ -2,16 +2,18 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME,
-});
-
 async function query(sql) {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT || 3306,
+        database: process.env.DB_NAME,
+    });
     const [results] = await connection.execute(sql);
+
+    await connection.end();
+
     return results;
 }
 
